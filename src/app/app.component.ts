f04e6f139acbd707d4f55076e0941559533a149b
Ginger Lee Kretschmer and Sailor Winkelman <ginger@>
2018-01-29 15:49:59 -0800

    refactor drawStuff function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 ///<reference path="./../../node_modules/p5/lib/p5.global-mode.d.ts" />
 const p5 = require('p5');
 import { Component, OnInit } from '@angular/core';
-// declare var p5: any;
-// import  p5  from 'p5';
 
 @Component({
   selector: 'app-root',
@@ -11,44 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'Canvas';
+  myp5 = p5;
 
-  ngOnInit(){
 
-    // createCanvas(200,200);
-    // this.setup();
-    // draw();
-
-    const sketch = function( p ) {
-
-      let x = 100;
-      let y = 100;
-
-      p.setup = function() {
-        p.createCanvas(700, 410);
+//initialize an instance of p5 canvas with drawing functions
+  drawStuff(){
+    const sketch = function( instance ) {
+    //create canvas and set bg color
+      instance.setup = function() {
+        instance.createCanvas(700, 410);
+        instance.background(51);
       };
-
-      p.draw = function() {
-        p.background(0);
-        p.fill(255);
-        p.rect(x,y,50,50);
+    //draw purple filled ellipse at mouse position
+      instance.draw = function() {
+        instance.fill(255, 0 , 200, 25);
+        instance.noStroke();
+        instance.ellipse(instance.mouseX, instance.mouseY, 48, 48);
       };
-    };
-    let myp5 = new p5(sketch);
-    console.log(myp5);
+    }; //sketch
+
+    this.myp5 = new p5(sketch);
+
+  }
+
+  deleteCanvas() {
+    this.myp5.remove();
   }
 
 
+  ngOnInit(){
+    this.drawStuff();
+    console.log(this.myp5);
+    setTimeout(this.myp5.deleteCanvas, 3000);
 
-
-  //
-  // setup() {
-  //   console.log("square canvas");
-  //   createCanvas(200,200);
-  // }
-  //
-  // draw() {
-  //   console.log("rectangle");
-  //   rect(50,50,20,20);
-  // }
-
+    //delete the canvas after a set interval
+  }
 }
